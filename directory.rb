d7cdb13421941aacadf0cccbcb93387320a3e0e6
Arif Ebrahim <>
2021-04-19 13:05:35 +0100

    refactored code

--- directory.rb
+++ directory.rb
@@ -1,19 +1,17 @@
+@students = []
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
   # get the first name
   name = gets.chomp
   # while name is not empty, repeat this code
   while !name.empty? do
     # add student hash to array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  # return the array of students
-  students 
 end
 
 def print_header
@@ -21,35 +19,45 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
+def print_student_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 def interactive_menu
-  students = []
   loop do
-    puts "1. input the students"
-    puts "2. show the students"
-    puts "9. exit"
-    selection = gets.chomp
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit
-    else
-      puts "i don't know what you mean"
-    end
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. input the students"
+  puts "2. show the students"
+  puts "9. exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "i don't know what you mean"
   end
 end
 
