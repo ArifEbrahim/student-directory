6b2bdac992b9ee26d9860b9a543881d84a828e0b
Arif Ebrahim <>
2021-04-23 12:33:25 +0100

    removed explicit file close

--- directory.rb
+++ directory.rb
@@ -70,22 +70,20 @@ def print_footer
 end
 
 def save_students(filename = "students.csv")
-  file = File.open(filename, "w")
+  file = File.open(filename, "w") do
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
   end
-  file.close
 end
 
 def load_students(filename = "students.csv")
-  file = File.open(filename, "r")
+  file = File.open(filename, "r") do
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
   end
-  file.close
   puts "Loaded #{@students.count} from #{filename}"
 end
 
